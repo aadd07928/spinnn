6d34d743a7426c1c0f82de148c9955317062ec60
aadd07928 <72698557+aadd07928@users.noreply.github.com>
2022-12-05 17:53:46 -0800

    Add files via upload

---
+++ spinnnnnn-2022-12-06T01-50-44/src/robot-config.cpp
@@ -0,0 +1,38 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor side = motor(PORT8, ratio18_1, true);
+controller Controller1 = controller();
+digital_out numatics = digital_out(Brain.ThreeWirePort.A);
+digital_out numatics2 = digital_out(Brain.ThreeWirePort.B);
+motor i = motor(PORT9, ratio6_1, false);
+motor fly1 = motor(PORT2, ratio6_1, true);
+motor fly2 = motor(PORT3, ratio6_1, true);
+optical Optic1 = optical(PORT6);
+optical Optic2 = optical(PORT4);
+motor fl = motor(PORT1, ratio18_1, false);
+motor fr = motor(PORT10, ratio18_1, true);
+motor bl = motor(PORT11, ratio18_1, false);
+motor br = motor(PORT20, ratio18_1, true);
+inertial inert = inertial(PORT7);
+rotation xrot = rotation(PORT5, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
